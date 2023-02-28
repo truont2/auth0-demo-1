99d1f7b859632ee293c7bd4e926d885e837cbc94
Takara Truong <takaraktruong@gmail.com>
2023-02-28 12:55:13 -0800

    basic auth0 setup complete

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+const LogoutButton = () => {
+  const { logout, isAuthenticated } = useAuth0();
+  return (
+    isAuthenticated && (
+      <button onClick={() => logout()}>SignOut</button>
+    )
+  );
+};
+
+export default LogoutButton;
